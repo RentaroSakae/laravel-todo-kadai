76d5de0e16dc9fc70bec09d71ae29d71a5320ef1
Rentaro Sakae <r.sakae0508@gmail.com>
2023-08-23 07:04:07 +0900

    初回コミット

---
+++ database/migrations/2023_08_21_104608_add_description_to_todos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('todos', function (Blueprint $table) {
+            $table->string('description', 200)->default("")->after('goal_id');//空文字を入れるをデフォルトにする　
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('todos', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
+    }
+};
